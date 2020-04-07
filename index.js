0bb0b3a71afec8ce185b9556d5600666a0bb50a6
Steve Farrar <consciencedub@yahoo.com>
2020-04-07 19:26:48 -0400

    footer fixed

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ jQuery(document).ready(function($) {
     $('.message').change(updateCountdown);
     $('.message').keyup(updateCountdown);
 });
+
+// function updateCountdown() {
+//     // 50 is the max message length
+//     var remaining = 50 - jQuery('.message').val().length;
+//     jQuery('.countdown').text(remaining + ' char. left');
+// }
+// jQuery(document).ready(function($) {
+//     updateCountdown();
+//     $('.message').change(updateCountdown);
+//     $('.message').keyup(updateCountdown);
+// });
+
+
+// var maxLength = 50;
+// $('textarea').keyup(function() {
+//   var length = $(this).val().length;
+//   var length = maxLength-length;
+//   $('#chars').text(length);
+// });
